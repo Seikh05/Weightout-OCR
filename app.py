d8a00eef08f4ae3494f2f94fa8db3ed025af58b6
Seikh Souvagya Mustakim <seikhsouvagyamustakim@gmail.com>
2025-10-06 16:40:00 +0530

    fixed ip adress for phone view

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def predict():
         return jsonify({"text": "Error processing image"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
